e326c5a4131d7d944648008d0ee6ed789f9bd30c
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2025-03-18 14:55:51 +0000

    added fluent validation

---
+++ Features/Validation/CreateUserValidator.cs
@@ -0,0 +1,14 @@
+﻿using _2025_epita_mediatr.Features.Command.User;
+using FluentValidation;
+
+namespace _2025_epita_mediatr.Features.Validation
+{
+    public class CreateUserValidator : AbstractValidator<CreateUserCommand>
+    {
+        public CreateUserValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().MinimumLength(15).WithMessage("Name is Required");
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
+using _2025_epita_mediatr.Features.Command.User;
+using FluentValidation;
+using FluentValidation.AspNetCore;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add MediatR
 builder.Services.AddMediatR(cfg=> cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
-
+builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddValidatorsFromAssemblies(new[] { Assembly.GetExecutingAssembly() });
 
 // Add services to the container.
 
@@ -13,6 +17,19 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+var serviceProvider = builder.Services.BuildServiceProvider();
+
+var validator = serviceProvider.GetRequiredService<IValidator<CreateUserCommand>>();
+
+if (validator is null)
+{
+    Console.WriteLine("Validation failed");
+}
+else
+{
+    Console.WriteLine("Validation passed");
+}
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
